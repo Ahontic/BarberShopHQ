4f57ceefb3acd60b0dee840af23b6e667690c65d
Quix <nswells14@gmail.com>
2019-02-08 11:57:29 +0200

    Saved client information advanced method

--- app.rb
+++ app.rb
@@ -28,19 +28,22 @@ post '/visit' do
 		# user_name, phone, date_time
 		@username 	= params[:username]
 		@phone 		= params[:phone]
-		@datetime 	= params[:datetime]
+		@datestamp 	= params[:datestamp]
 		@barber		= params[:barber]
 		@color		= params[:color]
 
 		# Сохранение в БД ламерский способ
 
-client = Client.new
-client.name      = @username
-client.phone     = @phone
-client.datestamp = @datetime
-client.barber    = @barber
-client.color     = @color
-client.save
+# client = Client.new
+# client.name      = @username
+# client.phone     = @phone
+# client.datestamp = @datetime
+# client.barber    = @barber
+# client.color     = @color
+# client.save
+
+c = Client.new params[:client]
+c.save
 		
 		erb "<h2> Спасибо, Вы записались!</h2>"
 		
