5fd2b839d4963ccd388e6b092fae83fd1762bf56
Quix <nswells14@gmail.com>
2019-02-08 13:05:27 +0200

    Added validation params[:client]

--- app.rb
+++ app.rb
@@ -6,7 +6,12 @@ require 'sinatra/activerecord'
 
 
 set :database, "sqlite3:barbershop.db"
+
 class Client < ActiveRecord::Base
+	validates :name, presence: true
+	validates :phone, presence: true
+	validates :datestamp, presence: true
+	validates :color, presence: true
 end
 
 class Barber < ActiveRecord::Base
@@ -26,16 +31,16 @@ end
 
 post '/visit' do
 		# user_name, phone, date_time
-		@username 	= params[:username]
-		@phone 		= params[:phone]
-		@datestamp 	= params[:datestamp]
-		@barber		= params[:barber]
-		@color		= params[:color]
+		# @name 		= params[:name]
+		# @phone 		= params[:phone]
+		# @datestamp 	= params[:datestamp]
+		# @barber		= params[:barber]
+		# @color		= params[:color]
 
 		# Сохранение в БД ламерский способ
 
 # client = Client.new
-# client.name      = @username
+# client.name      = @name
 # client.phone     = @phone
 # client.datestamp = @datetime
 # client.barber    = @barber
